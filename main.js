9fa12ae69af6af5f4b5adf7cab9fcd38743dd454
galoux537 <gabriel.ida@3cplusnow.com>
2025-02-27 19:53:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-// Este arquivo serve apenas como um redirecionador para o arquivo principal
-import './src/main.ts'; 
+// Conteúdo copiado de src/main.ts para resolver problemas de build
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './src/App.vue'
+import router from './src/router'
+import './src/assets/css/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app') 
