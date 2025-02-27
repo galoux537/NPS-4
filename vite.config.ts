e235cd5f47843570818449322d37596a4f8ebab5
galoux537 <gabriel.ida@3cplusnow.com>
2025-02-27 19:45:03 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,12 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
   }
 })
